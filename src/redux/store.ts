fd44ff4f75a1561d44b248d7bdd7c0244dcf25f2
munimulinfo <munimul.info@gmail.com>
2024-02-03 11:05:49 +0600

    crate authSlice and create userSet Global State function and Logout user function and redux persist install and current user rehydred every browser refresh relod this functionality implement

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./featuers/auth/authSlice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+const persistConfig = {
+  key: "auth",
+  storage,
+};
 
-export const store = configureStore({
-  reducer: {},
+const persistedReducer = persistReducer(persistConfig, authReducer);
+
+const store = configureStore({
+  reducer: {
+    auth: persistedReducer,
+  },
 });
 
 //handlle store typescript
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+export const persistor = persistStore(store);
+export default store;
